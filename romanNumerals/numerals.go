55264411893701b7bc94629eefbf71b125bf54e4
William Gough <williamhenrygough@gmail.com>
2017-07-05 13:11:03 +0100

    Add NumeralsToInts

--- romanNumerals/numerals.go
+++ romanNumerals/numerals.go
@@ -17,3 +17,36 @@ func Numerals(num int) string {
 	}
 	return "not found"
 }
+
+func divMod(quotient int, remainder int) {
+
+}
+
+// NumeralsToInts takes a roman numeral and returns an integer.
+func NumeralsToInts(input string) int {
+
+	var value int
+
+	numerals := map[rune]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+	}
+
+	switch input {
+	case "IV":
+		return 4
+	case "IX":
+		return 9
+	}
+
+	for _, char := range input {
+		for key, val := range numerals {
+			if char == key {
+				value += val
+			}
+		}
+	}
+
+	return value
+}
--- romanNumerals/numerals_test.go
+++ romanNumerals/numerals_test.go
@@ -11,3 +11,15 @@ func TestNumerals(t *testing.T) {
 	assert.Equal(t, "II", Numerals(2), "Returns roman numeral for 2 = II.")
 	assert.Equal(t, "not found", Numerals(53), "Returns not found for values outside of map.")
 }
+
+func TestNumeralsToInts(t *testing.T) {
+	assert.Equal(t, 1, NumeralsToInts("I"), "Returns integer for roman numeral I = 1")
+	assert.Equal(t, 2, NumeralsToInts("II"), "Returns integer for roman numeral II = 2")
+	assert.Equal(t, 4, NumeralsToInts("IV"), "Returns integer for roman numeral IV = 4")
+	assert.Equal(t, 5, NumeralsToInts("V"), "Returns integer for roman numeral V = 5")
+	assert.Equal(t, 6, NumeralsToInts("VI"), "Returns integer for roman numeral VI = 6")
+	assert.Equal(t, 7, NumeralsToInts("VII"), "Returns integer for roman numeral VII = 7")
+	assert.Equal(t, 9, NumeralsToInts("IX"), "Returns integer for roman numeral IX = 9")
+	assert.Equal(t, 9, NumeralsToInts("VIIII"), "Returns integer for roman numeral VIIII = 9")
+	assert.Equal(t, 11, NumeralsToInts("XI"), "Returns integer for roman numeral XI = 11")
+}
